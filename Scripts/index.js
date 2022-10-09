dd78558170a2f681b968425514b26a9fee7bb50a
Kartik Gupta <kartikguptampi@gmail.com>
2022-10-09 18:21:42 +0530

    cart component is added

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,13 +1,10 @@
 import navbar from "../components/navbar.js";
+import { cart } from "../components/cart.js";
+document.getElementById("cartinhtm").innerHTML = cart();
 document.getElementById("navbar").innerHTML = navbar();
-const opencartpage = () => {
-  console.log("ok");
-};
-// cartdiv.addEv/entListener("click", opencartpage);
 
 cartinhtm.addEventListener("click", () => {});
 cartdiv.addEventListener("click", () => {
-  //   showitemdetiabefor.id = "showitemdetiabefor";
   cartinhtm.id = "cartinhtml";
 });
 closeit.addEventListener("click", () => {
@@ -17,6 +14,25 @@ closeit.addEventListener("click", () => {
 const always = () => {
   let flag = "true";
   if (flag == "true") {
+    ` <h5><span>4</span> items</h5>
+    -
+    <h5><span> ₹458</span></h5>
+    <h5>Proceed <i class="fa-solid fa-right-to-bracket"></i></h5>`;
   } else if (flag == "false") {
   }
 };
+
+{
+  /* <div id="cartinhtm">
+      <div>
+        <div>
+          <h4>My Cart</h4>
+          <i class="fa-sharp fa-solid fa-xmark" id="closeit"></i>
+        </div>
+        <div></div>
+        <div id="showitemdetial">
+         
+        </div>
+      </div>
+    </div> */
+}
