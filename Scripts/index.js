653b886c17de2dadb0105f414f23f4f456b52be4
Kartik Gupta <kartikguptampi@gmail.com>
2022-10-09 18:05:45 +0530

    Navbar + cartPage

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,2 +1,22 @@
 import navbar from "../components/navbar.js";
 document.getElementById("navbar").innerHTML = navbar();
+const opencartpage = () => {
+  console.log("ok");
+};
+// cartdiv.addEv/entListener("click", opencartpage);
+
+cartinhtm.addEventListener("click", () => {});
+cartdiv.addEventListener("click", () => {
+  //   showitemdetiabefor.id = "showitemdetiabefor";
+  cartinhtm.id = "cartinhtml";
+});
+closeit.addEventListener("click", () => {
+  cartinhtml.id = "cartinhtm";
+});
+
+const always = () => {
+  let flag = "true";
+  if (flag == "true") {
+  } else if (flag == "false") {
+  }
+};
