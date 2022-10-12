4c99fb6d23d1c4b2a90f8be9be76c583fc04a59d
Abhinav Sinha <abhinavraj350@gmail.com>
2022-10-12 17:19:16 +0530

    donnnnnnneeeeeeeeeeee

--- Scripts/Freshvegitablemodule.js
+++ Scripts/Freshvegitablemodule.js
@@ -1,4 +1,47 @@
 import navbar from "../components/navbar.js";
-import { cart } from "../components/cart.js";
+import {
+  cart,
+  caraosual,
+  productsAllwithDetails,
+  DispalyAlliteminCart,
+  dataforinnercartcara,
+  x,
+  clickLeftButton,
+  clickRightButton,
+  clickonaddbtn,
+  subtractqty,
+  increaseqty,
+  always,
+} from "../components/cart.js";
+import {
+  users,
+  openonclick,
+  cancel,
+  login,
+  loginPage,
+  signupPage,
+  signUp,
+  currentupdate,
+  logoutcur,
+  mylocation,
+  getReverseGeocodingData,
+} from "../components/signup_login.js";
+window.clickonaddbtn = clickonaddbtn;
+window.subtractqty = subtractqty;
+window.increaseqty = increaseqty;
 document.getElementById("cartinhtm").innerHTML = cart();
-document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("navbar").innerHTML = navbar("../img/Untitled-1.jpg");
+currentupdate();
+mylocation();
+document.getElementById("logoutcurrent").addEventListener("click", logoutcur);
+
+const addpluschangehtml = () => {
+  console.log(event.target);
+};
+cartdiv.addEventListener("click", () => {
+  cartinhtm.id = "cartinhtml";
+  always();
+});
+document.getElementById("closeit").addEventListener("click", () => {
+  document.getElementById("cartinhtml").id = "cartinhtm";
+});
--- Scripts/everysearchmodule.js
+++ Scripts/everysearchmodule.js
@@ -2,11 +2,16 @@ import navbar from "../components/navbar.js";
 import {
   cart,
   caraosual,
+  productsAllwithDetails,
   DispalyAlliteminCart,
   dataforinnercartcara,
   x,
   clickLeftButton,
   clickRightButton,
+  clickonaddbtn,
+  subtractqty,
+  increaseqty,
+  always,
 } from "../components/cart.js";
 import {
   users,
@@ -21,13 +26,18 @@ import {
   mylocation,
   getReverseGeocodingData,
 } from "../components/signup_login.js";
-
+window.clickonaddbtn = clickonaddbtn;
+window.subtractqty = subtractqty;
+window.increaseqty = increaseqty;
 document.getElementById("cartinhtm").innerHTML = cart();
 document.getElementById("navbar").innerHTML = navbar("../img/Untitled-1.jpg");
 currentupdate();
 mylocation();
 document.getElementById("logoutcurrent").addEventListener("click", logoutcur);
 
+const addpluschangehtml = () => {
+  console.log(event.target);
+};
 cartdiv.addEventListener("click", () => {
   cartinhtm.id = "cartinhtml";
   always();
