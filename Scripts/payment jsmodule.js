5f86821f8f523a28654d8055933721ab7b0a99ad
Kartik Gupta <kartikguptampi@gmail.com>
2022-10-15 16:02:18 +0530

    Payment is added

---
+++ Scripts/payment jsmodule.js
@@ -0,0 +1,5 @@
+import navbar from "../components/navbar.js";
+// document.getElementById("cartinhtm").innerHTML = cart();
+import { footerhtml } from "../components/footer.js";
+document.getElementById("navbar").innerHTML = navbar("..");
+document.getElementById("footerindex").innerHTML = footerhtml("..");
